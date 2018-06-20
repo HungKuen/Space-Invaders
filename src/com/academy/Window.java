bf00f9e8ad1fbd3ff202783fce0037b68b29b5e0
Per- Fredrik Pettersson Boija <per_fredrik_80@hotmail.com>
2018-06-20 15:15:46 +0200

    vet inte vad som är fel

--- src/com/academy/Window.java
+++ src/com/academy/Window.java
@@ -1,22 +1,115 @@
 package com.academy;
 
+
+
 import javax.swing.*;
+
 import java.awt.*;
 
-public class Window extends Canvas {
+import java.awt.event.KeyEvent;
+
+import java.awt.event.KeyListener;
+
+
+
+public class Window implements KeyListener {
+
+
 
     public Window(int width, int height, String title, Game game){
-       JFrame frame = new JFrame(title);
+
+        JFrame frame = new JFrame(title);
+
+        frame.addKeyListener(this);
+
         frame.setPreferredSize((new Dimension(width, height)));
+
         frame.setMaximumSize((new Dimension(width, height)));
+
         frame.setMinimumSize((new Dimension(width, height)));
 
+
+
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
         frame.setResizable(false);
+
         frame.setLocationRelativeTo(null);
+
         frame.add(game);
+
         frame.setVisible(true);
+
         game.start();
+
+    }
+
+
+
+
+
+    @Override
+
+    public void keyTyped(KeyEvent e) {
+
+
+
+    }
+
+
+
+    @Override
+
+    public void keyPressed(KeyEvent e) {
+
+        if (e.getKeyCode() == KeyEvent.VK_SPACE){
+
+            System.out.println("Fire"); // kod för att skjuta
+
+        }
+
+        if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+            Player.setX(Player.getX()-1);
+            System.out.println("Left"); // styra vänster
+
+        }
+
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT){
+            Player.setX(Player.getX()+1);
+            System.out.println("Right"); // styra höger
+
+        }
+
+/*      Komande patch
+
+
+
+        if (e.getKeyCode() == KeyEvent.VK_UP){
+
+            System.out.println("gamespeed up");
+
+        }
+
+        if (e.getKeyCode() == KeyEvent.VK_DOWN){
+
+            System.out.println("gamespeed down");
+
+        }
+
+*/
+
+
+
+    }
+
+
+
+    @Override
+
+    public void keyReleased(KeyEvent e) {
+
+
+
     }
 
 }
